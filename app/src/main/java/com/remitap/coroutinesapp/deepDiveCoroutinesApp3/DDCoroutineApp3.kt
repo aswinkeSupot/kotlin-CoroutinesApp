1d4fc85b8e47fe42f50c57342faee59e7bfb5154
aswin ke <aswinke@aswins-MacBook-Air.local>
2024-10-02 13:21:26 +0530

    deepDiveCoroutines App 3 Finish

--- app/src/main/java/com/remitap/coroutinesapp/deepDiveCoroutinesApp3/DDCoroutineApp3.kt
+++ app/src/main/java/com/remitap/coroutinesapp/deepDiveCoroutinesApp3/DDCoroutineApp3.kt
@@ -1,6 +1,7 @@
 package com.remitap.coroutinesapp.deepDiveCoroutinesApp3
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,7 @@ import com.remitap.coroutinesapp.databinding.ActivityDdcoroutineApp2Binding
 import com.remitap.coroutinesapp.databinding.ActivityDdcoroutineApp3Binding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -22,33 +24,47 @@ class DDCoroutineApp3 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_ddcoroutine_app3)
 
-        binding.btnCount.setOnClickListener {
+        // Example for Concurent/Sequential Coroutine
+        binding.btnSequential.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                Log.v("TAGGY","CoroutineSequential Starting")
+                val one = doSomeThingUsefulOne()
+                Log.v("TAGGY","Coroutine 1 Finished")
+                val two = doSomeThingUsefulTwo()
+                Log.v("TAGGY","Coroutine 2 Finished")
 
+                val result =one + two
+                Log.v("TAGGY", "The Result is $result")
+            }
         }
 
-        binding.btnDownload.setOnClickListener {
+        // Example for Parallel Coroutine
+        binding.btnParallel.setOnClickListener {
 
+            CoroutineScope(Dispatchers.IO).launch {
+                Log.v("TAGGY","CoroutineParallel Starting")
+                val one = async { doSomeThingUsefulOne() }
+                Log.v("TAGGY","CoroutineParallel 1 Finished")
+                val two = async { doSomeThingUsefulTwo() }
+                Log.v("TAGGY","CoroutineParallel 2 Finished")
 
+                val result =one.await() + two.await()
+                Log.v("TAGGY", "The Result is $result")
+            }
         }
 
     }
 
     suspend fun doSomeThingUsefulOne(): Int {
         delay(1000L)
+        Log.v("TAGGY", "Starting Coroutine 1")
         return 5
     }
 
     suspend fun doSomeThingUsefulTwo(): Int {
         delay(3000L)
+        Log.v("TAGGY", "Starting Coroutine 2")
         return 10
     }
 
-    private suspend fun downloadBigFileFromNet() {
-        for (i in 1..1000000) {
-            withContext(Dispatchers.Main){
-                binding.tvDownloadBigFile.text = "$i"
-            }
-
-        }
-    }
 }
